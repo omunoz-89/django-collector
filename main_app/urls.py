9ba4fcace767dc7a50cd31f405ec7eb1d6673197
omunoz <steven.120189@gmail.com>
2021-06-23 16:35:30 -0400

    changes made

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('', views.index, name='index'),
     path('cats/', views.cats_index, name='cats_index'),
-    path('cats/<int:cat_id>', views.cats_show, name='cats_show')
+    path('cats/<int:cat_id>', views.cats_show, name='cats_show'),
+    path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.save()
+    return HttpResponseRedirect('/cats/' + str(self.object.pk))
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
 
 # Create your views here.
 def about(request):
